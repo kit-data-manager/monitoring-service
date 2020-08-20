144a5eeaf0688dad2b415da7d8143eb841b3ed28
ThomasJejkal <thomas.jejkal@kit.edu>
2020-08-20 13:00:03 +0200

    Added code for admin-service Removed/replaced template elements

---
+++ src/main/java/edu/kit/datamanager/admin/configuration/SecuritySecureConfig.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kit.datamanager.admin.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+/**
+ *
+ * @author Torridity
+ */
+@Configuration
+public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler
+                = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl("/");
+
+        http.authorizeRequests()
+                .antMatchers("/assets/**").permitAll()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin().loginPage("/login")
+                .successHandler(successHandler).and()
+                .logout().logoutUrl("/logout").and()
+                .httpBasic().and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers(
+                        "/instances",
+                        "/actuator/**"
+                );
+    }
+}
